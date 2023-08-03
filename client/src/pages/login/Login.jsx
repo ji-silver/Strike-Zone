c28033ec1054c6c5125969499bfe566c76f5d6e9
jisilver <jisilver.kim@gmail.com>
2023-08-04 01:02:27 +0900

    Feat: 로그인 기능 구현

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,19 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./login.scss";
 import FormData from "../../components/formData/FormData";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const [errors, setErrors] = useState("");
+  const [userValue, setUserValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserValue((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    if (!userValue.email) {
+      setErrors((prev) => ({ ...prev, email: "이메일을 입력해주세요." }));
+    }
+    if (!userValue.password) {
+      setErrors((prev) => ({ ...prev, password: "비밀번호를 입력해주세요." }));
+    }
+
+    if (!userValue.email || !userValue.password) {
+      return;
+    }
+
+    try {
+      await axios.post("/auth/login", userValue);
+      alert("로그인 되었습니다.");
+      navigate("/");
+    } catch (err) {
+      setErrors({ password: "이메일 혹은 비밀번호가 일치하지 않습니다." });
+    }
+  };
+
   return (
     <>
       <FormData>
         <div className="login_bottom">
           <div className="formInput">
-            <input type="text" placeholder="이메일" />
-            <input type="password" placeholder="비밀번호" />
+            <input
+              type="text"
+              placeholder="이메일"
+              name="email"
+              onChange={handleChange}
+            />
+            {errors.email && <span className="errMessage">{errors.email}</span>}
+            <input
+              type="password"
+              placeholder="비밀번호"
+              name="password"
+              onChange={handleChange}
+            />
+            {errors && <span className="errMessage">{errors.password}</span>}
           </div>
           <div className="btn">
-            <button>Login</button>
+            <button onClick={handleClick}>Login</button>
           </div>
           <div className="desc">
             <span>아직 계정이 없으신가요?</span>
