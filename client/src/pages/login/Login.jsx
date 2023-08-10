52be754a56e856f42e143e0b9909dc56a3c6d85d
jisilver <jisilver.kim@gmail.com>
2023-08-11 00:16:58 +0900

    Fix

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -7,6 +7,7 @@ import { useDispatch } from "react-redux";
 import { login } from "../../redux/userSlice";
 
 const Login = () => {
+  const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
   const [errors, setErrors] = useState("");
   const [userValue, setUserValue] = useState({
     email: "",
@@ -36,7 +37,7 @@ const Login = () => {
     }
 
     try {
-      const res = await axios.post("/auth/login", userValue);
+      const res = await axios.post(`${PROXY}/auth/login`, userValue);
       dispatch(login(res.data));
       navigate("/");
     } catch (err) {
